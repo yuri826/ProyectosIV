1b11c036d81866b5871aef2c611f04a47ec76003
yuri826 <kopoflakes@gmail.com>
2026-02-16 15:30:34 +0100

    Mi pana se mueve

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private CharacterController characterController;
+
+    private Vector2 movementInputDirection;
+
+    [Header("Movement")] 
+    [SerializeField] private float walkSpeed;
+    private Vector3 movementDirection;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void OnEnable()
+    {
+        playerInput.actions["Move"].performed += UpdateMovementInput;
+        playerInput.actions["Move"].canceled += UpdateMovementInput;
+    }
+    
+    private void OnDisable()
+    {
+        playerInput.actions["Move"].performed -= UpdateMovementInput;
+        playerInput.actions["Move"].canceled -= UpdateMovementInput;
+    }
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        movementDirection = new Vector3(movementInputDirection.x, 0f, movementInputDirection.y);
+        characterController.Move(movementDirection * (walkSpeed * Time.deltaTime));
+    }
+
+    private void UpdateMovementInput(InputAction.CallbackContext obj)
+    {
+        movementInputDirection = obj.ReadValue<Vector2>();
+    }
+}
