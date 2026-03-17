f997a32f6594e0a2d77d45ca0fdd1cfa9af5d00a
JacoboHerrero <jaco.herrero@gmail.com>
2026-03-17 02:02:05 +0100

    Enums

--- Assets/Scripts/Enemies/SabotagePointState.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public enum SabotagePointState
-{
-    Free,
-    Reserved,
-    Broken
-}
---
+++ Assets/Scripts/Enums/GameEnums.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public enum PlayerState
+{
+    Locked,
+    Move,
+    Dash,
+    Repair,
+    GameOver
+}
+
+public enum DepositState
+{
+    Objects,
+    Tool,
+    Completed
+}
+
+public enum LevelFlowState
+{
+    Intro,
+    Gameplay,
+    Win,
+    GameOver
+}
+
+public enum SpeedState
+{
+    Low,
+    Middle,
+    High
+}
+
+public enum MapNodeState
+{
+    Locked,
+    Unlocked,
+    Completed
+}
+
+public enum OutlawState
+{
+    PickSabotage,
+    MoveToSabotage,
+    PlantDynamite,
+    MoveToSafePosition,
+    WaitExplosion,
+    Patrol,
+    Combat,
+    Laugh,
+    Sandstorm
+}
+
+public enum SabotagePointState
+{
+    Free,
+    Reserved,
+    Broken
+}
