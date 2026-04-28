ed051ecdf2c468972e5289a97587964ac1e0918e
yuri826 <kopoflakes@gmail.com>
2026-04-28 17:44:06 +0200

    LevelProgressDepricated

---
+++ Assets/Scripts/DEPRICATED/LevelProgressEventIconsHUD.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelProgressEventIconsHUD : MonoBehaviour
+{
+    //Depricated
+    
+    // [Header("References")]
+    // [SerializeField] private RectTransform progressIconsRoot;
+    // [SerializeField] private LevelProgressEventIconItem iconPrefab;
+    //
+    // [Header("Layout")]
+    // [SerializeField] private float progressBarWidth = 500f;
+    //
+    // private readonly List<LevelProgressEventIconItem> spawnedIcons = new();
+    //
+    // private void Start()
+    // {
+    //     BuildIcons();
+    // }
+    //
+    // private void BuildIcons()
+    // {
+    //     int levelDuration = TrainGameMode.instance.levelFlow.levelDuration;
+    //     LevelEventInfo[] levelEvents = TrainGameMode.instance.GetLevelEventSubsystem().levelEvents;
+    //
+    //     foreach (LevelEventInfo eventInfo in levelEvents)
+    //     {
+    //         LevelProgressEventIconItem iconItem = Instantiate(iconPrefab, progressIconsRoot);
+    //         spawnedIcons.Add(iconItem);
+    //
+    //         RectTransform iconRect = iconItem.GetComponent<RectTransform>();
+    //
+    //         float normalizedTime = Mathf.Clamp01((float)eventInfo.execTime / levelDuration);
+    //         float xPosition = normalizedTime * progressBarWidth;
+    //
+    //         iconRect.anchoredPosition = new Vector2(xPosition, iconRect.anchoredPosition.y);
+    //
+    //         iconItem.Initialize();
+    //     }
+    // }
+}
